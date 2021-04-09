f7ef8f29fed4389e16012c3042421fcda7b0b9f2
Simon Towler <simon.towler@icloud.com>
2021-04-09 12:36:17 +0100

    Refactor to import func and data to display list of to-do items

--- todo_app/app.py
+++ todo_app/app.py
@@ -1,12 +1,6 @@
 from flask import Flask, render_template, session
-
-
 from todo_app.flask_config import Config
-
-_DEFAULT_ITEMS = [
-    { 'id': 1, 'status': 'Not Started', 'title': 'List saved todo items' },
-    { 'id': 2, 'status': 'Not Started', 'title': 'Allow new items to be added' }
-]
+from todo_app.data.session_items import _DEFAULT_ITEMS, get_items
 
 app = Flask(__name__)
 app.config.from_object(Config)
@@ -15,8 +9,6 @@ app.config.from_object(Config)
 @app.route('/')
 def index():
     # return 'Hello World!'
-    def get_items():
-        return session.get('items', _DEFAULT_ITEMS)
     return render_template('index.html', items=get_items())
 
 

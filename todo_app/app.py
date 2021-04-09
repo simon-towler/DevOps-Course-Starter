b7003b0af64910ae7925c370fd0339780f20f831
Simon Towler <simon.towler@icloud.com>
2021-04-09 12:04:38 +0100

    Display the list of saved to-do items no the page

--- todo_app/app.py
+++ todo_app/app.py
@@ -1,6 +1,11 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, session
 
 from todo_app.flask_config import Config
+
+_DEFAULT_ITEMS = [
+    { 'id': 1, 'status': 'Not Started', 'title': 'List saved todo items' },
+    { 'id': 2, 'status': 'Not Started', 'title': 'Allow new items to be added' }
+]
 
 app = Flask(__name__)
 app.config.from_object(Config)
@@ -9,7 +14,9 @@ app.config.from_object(Config)
 @app.route('/')
 def index():
     # return 'Hello World!'
-    return render_template('index.html')
+    def get_items():
+        return session.get('items', _DEFAULT_ITEMS)
+    return render_template('index.html', items=get_items())
 
 
 if __name__ == '__main__':

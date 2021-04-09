50769c46d90cef7fdfc3f319c71cd95ef7d6cbc9
Simon Towler <simon.towler@icloud.com>
2021-04-09 16:28:22 +0100

    Fix issue 6 bug: After item is added url is wrong. Refreshing page will add item again.

--- todo_app/app.py
+++ todo_app/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, session, request
+from flask import Flask, render_template, session, request, redirect
 from todo_app.flask_config import Config
 from todo_app.data.session_items import _DEFAULT_ITEMS, get_items, add_item
 
@@ -15,7 +15,7 @@ def index():
 def addItem():
     itemName = request.form['itemName']
     add_item(itemName)
-    return render_template('index.html', items=get_items())
+    return redirect("/", code=303)
 
 if __name__ == '__main__':
     app.run()

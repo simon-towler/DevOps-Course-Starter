4f2eba0202a9a302d57e58c578a060962969d584
Simon Towler <simon.towler@icloud.com>
2021-04-09 15:51:37 +0100

    Be able to add new Items to the todo list

--- todo_app/app.py
+++ todo_app/app.py
@@ -1,6 +1,6 @@
-from flask import Flask, render_template, session
+from flask import Flask, render_template, session, request
 from todo_app.flask_config import Config
-from todo_app.data.session_items import _DEFAULT_ITEMS, get_items
+from todo_app.data.session_items import _DEFAULT_ITEMS, get_items, add_item
 
 app = Flask(__name__)
 app.config.from_object(Config)
@@ -11,6 +11,11 @@ def index():
     # return 'Hello World!'
     return render_template('index.html', items=get_items())
 
+@app.route('/addItem', methods=['POST'])
+def addItem():
+    itemName = request.form['itemName']
+    add_item(itemName)
+    return render_template('index.html', items=get_items())
 
 if __name__ == '__main__':
     app.run()

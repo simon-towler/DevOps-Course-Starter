86f56bae63ce8f81ab40e52bee9d83395238e605
Simon Towler <simon.towler@icloud.com>
2021-05-07 20:44:48 +0100

    Get Trello key and token from the environment

--- todo_app/app.py
+++ todo_app/app.py
@@ -2,20 +2,23 @@ from flask import Flask, render_template, session, request, redirect
 from todo_app.flask_config import Config
 from todo_app.data.session_items import _DEFAULT_ITEMS, get_items, add_item
 import requests
+import os
 
 app = Flask(__name__)
 app.config.from_object(Config)
+TRELLO_KEY = os.environ.get('TRELLO_KEY')
+TRELLO_TOKEN = os.environ.get('TRELLO_TOKEN')
 
 
 @app.route('/')
 def index():
-    response = requests.get("https://trello.com/1/lists/609392e0aaa6e8618e341f90/cards?key=58e57e7addabdcbcfae7f8d62560afbe&token=a9f6f553a13eceaf9eda2839cd85b665638644ca3c36011ccb18545f5325cd8d")
+    response = requests.get(f"https://trello.com/1/lists/609392e0aaa6e8618e341f90/cards?key={TRELLO_KEY}&token={TRELLO_TOKEN}")
     return render_template('index.html', items=response.json())
 
 @app.route('/addItem', methods=['POST'])
 def addItem():
     itemName = request.form['name']
-    requests.post("https://trello.com/1/cards?key=58e57e7addabdcbcfae7f8d62560afbe&token=a9f6f553a13eceaf9eda2839cd85b665638644ca3c36011ccb18545f5325cd8d&idList=609392e0aaa6e8618e341f90", data={"name":itemName})
+    requests.post(f"https://trello.com/1/cards?key={TRELLO_KEY}&token={TRELLO_TOKEN}&idList=609392e0aaa6e8618e341f90", data={"name":itemName})
     return redirect("/", code=303)
 
 if __name__ == '__main__':

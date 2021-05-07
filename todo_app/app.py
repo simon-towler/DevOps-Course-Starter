1772014f2f7b989f1ad2da4b5202d97d56ec0cfc
Simon Towler <simon.towler@icloud.com>
2021-05-07 18:32:55 +0100

    Get items from Trello instead of from cookie

--- todo_app/app.py
+++ todo_app/app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, session, request, redirect
 from todo_app.flask_config import Config
 from todo_app.data.session_items import _DEFAULT_ITEMS, get_items, add_item
+import requests
 
 app = Flask(__name__)
 app.config.from_object(Config)
@@ -8,7 +9,7 @@ app.config.from_object(Config)
 
 @app.route('/')
 def index():
-    return render_template('index.html', items=get_items())
+    return render_template('index.html', items=requests.get("https://trello.com/1/lists/609392e0aaa6e8618e341f90/cards?key=58e57e7addabdcbcfae7f8d62560afbe&token=a9f6f553a13eceaf9eda2839cd85b665638644ca3c36011ccb18545f5325cd8d"))
 
 @app.route('/addItem', methods=['POST'])
 def addItem():

68910a227ae7e8ddc61f5ab11fba9f4f157219f0
Simon Towler <simon.towler@icloud.com>
2021-05-08 20:57:31 +0100

    Use variables for Trello list ids

--- todo_app/app.py
+++ todo_app/app.py
@@ -8,17 +8,18 @@ app = Flask(__name__)
 app.config.from_object(Config)
 TRELLO_KEY = os.environ.get('TRELLO_KEY')
 TRELLO_TOKEN = os.environ.get('TRELLO_TOKEN')
+TRELLO_LIST_ID_DOING = "609392e0aaa6e8618e341f90"
 
 
 @app.route('/')
 def index():
-    response = requests.get(f"https://trello.com/1/lists/609392e0aaa6e8618e341f90/cards?key={TRELLO_KEY}&token={TRELLO_TOKEN}")
+    response = requests.get(f"https://trello.com/1/lists/{TRELLO_LIST_ID_DOING}/cards?key={TRELLO_KEY}&token={TRELLO_TOKEN}")
     return render_template('index.html', items=response.json())
 
 @app.route('/addItem', methods=['POST'])
 def addItem():
     itemName = request.form['name']
-    requests.post(f"https://trello.com/1/cards?key={TRELLO_KEY}&token={TRELLO_TOKEN}&idList=609392e0aaa6e8618e341f90", data={"name":itemName})
+    requests.post(f"https://trello.com/1/cards?key={TRELLO_KEY}&token={TRELLO_TOKEN}&idList={TRELLO_LIST_ID_DOING}", data={"name":itemName})
     return redirect("/", code=303)
 
 if __name__ == '__main__':
